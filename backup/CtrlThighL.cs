405b7b3f079d2e9cd1d5be3e8d6fa0fe820bdec0
yul10001 <51995675+yul10001@users.noreply.github.com>
2021-11-05 21:41:34 +0900

    기본 씬 및 스크립트

---
+++ backup/CtrlThighL.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlThighL : MonoBehaviour
+{
+    HingeJoint hingeJoint;
+    JointMotor jointMotor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hingeJoint = this.GetComponent<HingeJoint>();
+        jointMotor = hingeJoint.motor;
+        jointMotor.targetVelocity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {      
+            RollIn();   
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            RollOut();
+        }
+
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Q))
+        {
+            RollStop();
+        }
+        //    if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Q))
+        //    {
+        //        RollStop();
+        //    }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Q))
+        {
+            RollStop();
+        }
+    }
+
+    void RollIn()
+    {
+        jointMotor.targetVelocity = 100;
+        hingeJoint.motor = jointMotor;
+        Debug.Log("in");
+    }
+    void RollOut()
+    {
+        jointMotor.targetVelocity = -100;
+        hingeJoint.motor = jointMotor;
+        Debug.Log("out");
+    }
+    void RollStop()
+    {
+        jointMotor.targetVelocity = 0;
+        hingeJoint.motor = jointMotor;
+        Debug.Log("stop");
+
+    }
+}
---
+++ backup/CtrlThighR.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlThighR : MonoBehaviour
+{
+    HingeJoint hingeJoint;
+    JointMotor jointMotor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hingeJoint = this.GetComponent<HingeJoint>();
+        jointMotor = hingeJoint.motor;
+        jointMotor.targetVelocity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightBracket))
+        {
+            RollIn();
+        }
+
+        if (Input.GetKey(KeyCode.LeftBracket))
+        {
+            RollOut();
+        }
+
+        if (Input.GetKey(KeyCode.LeftBracket) && Input.GetKey(KeyCode.RightBracket))
+        {
+            RollStop();
+        }
+        //    if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Q))
+        //    {
+        //        RollStop();
+        //    }
+        if (Input.GetKeyUp(KeyCode.LeftBracket) || Input.GetKeyUp(KeyCode.RightBracket))
+        {
+            RollStop();
+        }
+    }
+
+    void RollIn()
+    {
+        jointMotor.targetVelocity = 100;
+        hingeJoint.motor = jointMotor;
+        Debug.Log("in");
+    }
+    void RollOut()
+    {
+        jointMotor.targetVelocity = -100;
+        hingeJoint.motor = jointMotor;
+        Debug.Log("out");
+    }
+    void RollStop()
+    {
+        jointMotor.targetVelocity = 0;
+        hingeJoint.motor = jointMotor;
+        Debug.Log("stop");
+
+    }
+}
