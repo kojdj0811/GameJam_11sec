6e9dc98091e26ecba26e4cb2f377f8289cec1e23
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2021-11-07 03:20:12 +0900

    Ui 조작키 / 설명 판넬

---
+++ Assets/NSY/Script/MapText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapText : MonoBehaviour
+{
+    public GameObject MapTexting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startLine());
+    }
+    IEnumerator startLine()
+    {
+      
+        MapTexting.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Debug.Log("»ç¶óÁü");
+        MapTexting.SetActive(false);
+    }
+  
+}
