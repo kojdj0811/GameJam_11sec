ce0ef8c3e61182dd1984212febccbb0025589631
yul10001 <51995675+yul10001@users.noreply.github.com>
2021-11-07 03:13:57 +0900

    '폐기' 뜨게 수정, 레벨에 적용

--- Assets/sycho/FailReload.cs
+++ Assets/sycho/FailReload.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FailReload : MonoBehaviour
 {
     public float speed;
     public GameObject m_pelvic;
+    public GameObject m_gameoverPanel;
+    public GameObject m_gameoverText;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 4000f;
+        m_gameoverPanel = GameObject.Find("GameoverPanel");
+        m_gameoverText = GameObject.Find("GameoverText");
+        m_gameoverPanel.GetComponent<Image>().enabled = false;
+        m_gameoverText.GetComponent<Text>().enabled = false;
     }
 
     // Update is called once per frame
@@ -32,6 +39,8 @@ public class FailReload : MonoBehaviour
     }
     public void ThrowAway()
     {
+        m_gameoverPanel.GetComponent<Image>().enabled = true;
+        m_gameoverText.GetComponent<Text>().enabled = true;
         m_pelvic.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         m_pelvic.GetComponent<Rigidbody>().AddForce(new Vector3(1, 1, 0) * speed);
     }
