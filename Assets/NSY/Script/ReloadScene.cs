7890ab06b18e4295ed3b4dbe2860b090ac2c0bc5
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2021-11-06 18:46:58 +0900

    다시하기/R누르면 다시하기

--- Assets/NSY/Script/ReloadScene.cs
+++ Assets/NSY/Script/ReloadScene.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ReloadScene : MonoBehaviour
 {
    
 
     // Update is called once per frame
-    void Update()
-    {
-        ReLoad();
-    }
+    
 
-    public void ReLoad()
+    public void ClickReloadBtn()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
