ac4684e00c65845b7d74bf7424785197133fef40
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2021-11-06 13:26:21 +0900

    이동방해 큐브 , 클리어 큐브

--- Assets/NSY/Script/PushBlock.cs
+++ Assets/NSY/Script/PushBlock.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PushBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Rigidbody rid;
+    [Header("¹Ì´ÂÈû")]
+    public float ForceMagnitude;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        
+        rid = hit.collider.attachedRigidbody;
+        if (rid !=null)
+        {
+            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+            forceDirection.y = 0.0f;
+            forceDirection.Normalize();
+
+            rid.AddForceAtPosition(forceDirection * ForceMagnitude, transform.position, ForceMode.Impulse);
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
